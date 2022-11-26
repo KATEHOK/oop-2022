426cfcecffffe1a772b8be81631debd3d9f10f43
Katehok <a.timin2003@mail.ru>
2022-11-26 14:31:08 +0300

    Added comments in doxygen in group.h

--- 4/simple/4/src/group.h
+++ 4/simple/4/src/group.h
@@ -8,50 +8,118 @@
 
 namespace group {
 
+	/**
+	* @brief Абстрактный класс, описывающий группу
+	*/
 	class Group {
 
 		friend class GroupsTableItem;
 		friend class GroupsTable;
 
 	protected:
-		int _id; // уникален в рамках одной таблицы
-		int _size; // количество студентов
-		int _department_id;
-		int _study_duration; // срок обучения
 
+		//! Номер группы (уникален в рамках одной таблицы)
+		int _id;
+
+		//! Количество студентов группы
+		int _size;
+
+		//! Номер профилирующей кафедры
+		int _department_id;
+
+		//! срок обучения
+		int _study_duration;
+
+		/**
+		* @brief Функция вывода абстрактной группы в стандартный поток вывода
+		* @param out Ссылка на стандартный поток вывода
+		*/
 		void output(std::ostream& out) const;
 
 	public:
+
+		/**
+		* @brief Конструктор по значениям полей
+		* @param id Номер группы
+		* @param size Количество студентов группы
+		* @param department_id Номер профилирующей кафедры
+		* @param study_duration Срок обучения
+		*/
 		Group(int id, int size, int department_id, int study_duration) :
 			_id(id),
 			_size(size),
 			_department_id(department_id),
 			_study_duration(study_duration) {}
 
+		/**
+		* @brief Копирующий конструктор
+		* @param src Ссылка на копируемый объект абстрактной группы
+		*/
 		Group(const Group& src) :
 			_id(src._id),
 			_size(src._size),
 			_department_id(src._department_id),
 			_study_duration(src._study_duration) {}
 
+		/**
+		* @brief Геттер номера группы
+		* @return Номер группы
+		*/
 		int id() const;
 
+		/**
+		* @brief Геттер количества студентов группы
+		* @return Количество студентов группы
+		*/
 		int size() const;
 
+		/**
+		* @brief Сеттер количества студентов группы
+		* @param size Новое количество студентов группы
+		* @return Количество студентов группы
+		*/
 		int size(int size);
 
+		/**
+		* @brief Геттер номера профилирующей кафедры
+		* @return Номер профилирующей кафедры
+		*/
 		int department_id() const;
 
+		/**
+		* @brief Геттер срока обучения
+		* @return Срок обучения
+		*/
 		int study_duration() const;
 	};
 
+	/**
+	* @brief Класс дневной группы
+	*/
 	class DayGroup : public Group {
 	private:
+
+		//! Специализация группы
 		std::string _specialization;
-		float _stipend; // размер стипендии
-		int _fellows_amount; // количество стипендиатов
+
+		//! Размер стипендии в группе
+		float _stipend;
+
+		//! Количество стипендиатов в группе
+		int _fellows_amount;
 
 	public:
+
+		/**
+		* @brief Конструктор по значениям полей
+		* @param id Номер группы
+		* @param size Количество студентов группы
+		* @param department_id Номер профилирующей кафедры
+		* @param study_duration Срок обучения
+		* @param specialization Специализация группы
+		* @param stipend Размер стипендии в группе
+		* @param fellows_amount Количество стипендиатов в группе
+		*/
 		DayGroup(int id, int size, int department_id, int study_duration,
 			std::string specialization, float stipend, int fellows_amount) :
 			Group(id, size, department_id, study_duration),
@@ -59,12 +127,20 @@ namespace group {
 			_stipend(stipend),
 			_fellows_amount(fellows_amount) {}
 
+		/**
+		* @brief Копирующий конструктор
+		* @param src Ссылка на копируемый объект группы
+		*/
 		DayGroup(const DayGroup& src) :
 			Group(src),
 			_specialization(src._specialization),
 			_stipend(src._stipend),
 			_fellows_amount(src._fellows_amount) {}
 
+		/**
+		* @brief Перемещающий конструктор
+		* @param src Ссылка на перемещаемый объект группы
+		*/
 		DayGroup(DayGroup&& src) :
 			Group(src),
 			_specialization(src._specialization),
@@ -73,40 +149,103 @@ namespace group {
 			if (this != &src) src._specialization.clear();
 		}
 
+		/**
+		* @brief Копирующий оператор присваивания
+		* @param src Ссылка на копируемый объект группы
+		* @return Ссылка на скопированный объект
+		*/
 		DayGroup& operator= (const DayGroup& src);
 
+		/**
+		* @brief Перемещающий оператор присваивания
+		* @param src Ссылка на перемещаемый объект группы
+		* @return Ссылка на перемещенный объект
+		*/
 		DayGroup& operator= (DayGroup&& src);
 
+		/**
+		* @brief Геттер специализации
+		* @return Специализация
+		*/
 		std::string specialization() const;
 
+		/**
+		* @brief Геттер размера стипендии
+		* @return Размер стипендии
+		*/
 		float stipend() const;
 
+		/**
+		* @brief Сеттер размера стипендии
+		* @param stipend размер стипендии
+		* @return Размер стипендии
+		*/
 		float stipend(float stipend);
 
+		/**
+		* @brief Геттер количества стипендиатов
+		* @return Количество стипендиатов
+		*/
 		int fellows_amount() const;
 
+		/**
+		* @brief Сеттер количества стипендиатов
+		* @param Количество стипендиатов
+		* @return Количество стипендиатов
+		*/
 		int fellows_amount(int amount);
 
+		/**
+		* @brief Дружественный оператор вывода дневной группы в стандартный поток вывода
+		* @param out Ссылка на стандартный поток вывода
+		* @param dg Ссылка на объект дневной группы
+		* @return Ссылка на стандартный поток вывода
+		*/
 		friend std::ostream& operator<< (std::ostream& out, const DayGroup& dg);
 	};
 
+	/**
+	* @brief Класс вечерней группы
+	*/
 	class EveningGroup : public Group {
 	private:
+
+		//! Контингент
 		std::string _contingent;
+
+		//! Квалификация
 		std::string _qualification;
 
 	public:
+
+		/**
+		* @brief Конструктор по значениям полей
+		* @param id Номер группы
+		* @param size Количество студентов группы
+		* @param department_id Номер профилирующей кафедры
+		* @param study_duration Срок обучения
+		* @param contingent Контингент группы
+		* @param qualification Квалификация группы
+		*/
 		EveningGroup(int id, int size, int department_id, int study_duration,
 			std::string contingent, std::string qualification) :
 			Group(id, size, department_id, study_duration),
 			_contingent(contingent),
 			_qualification(qualification) {}
 
+		/**
+		* @brief Копирующий конструктор
+		* @param src Ссылка на копируемый объект группы
+		*/
 		EveningGroup(const EveningGroup& src) :
 			Group(src),
 			_contingent(src._contingent),
 			_qualification(src._qualification) {}
 
+		/**
+		* @brief Перемещающий конструктор
+		* @param src Ссылка на перемещаемый объект группы
+		*/
 		EveningGroup(EveningGroup&& src) :
 			Group(src),
 			_contingent(src._contingent),
@@ -117,49 +256,127 @@ namespace group {
 			}
 		}
 
+		/**
+		* @brief Копирующий оператор присваивания
+		* @param src Ссылка на копируемый объект группы
+		* @return Ссылка на скопированный объект
+		*/
 		EveningGroup& operator= (const EveningGroup& src);
 
+		/**
+		* @brief Перемещающий оператор присваивания
+		* @param src Ссылка на перемещаемый объект группы
+		* @return Ссылка на перемещенный объект
+		*/
 		EveningGroup& operator= (EveningGroup&& src);
 
+		/**
+		* @brief Геттер контингента
+		* @return Контингент
+		*/
 		std::string contingent() const;
 
+		/**
+		* @brief Геттер квалификации
+		* @return Квалификация
+		*/
 		std::string qualification() const;
 
+		/**
+		* @brief Дружественный оператор вывода вечерней группы в стандартный поток вывода
+		* @param out Ссылка на стандартный поток вывода
+		* @param eg Ссылка на объект вечерней группы
+		* @return Ссылка на стандартный поток вывода
+		*/
 		friend std::ostream& operator<< (std::ostream& out, const EveningGroup& eg);
 	};
 
+	/**
+	* @brief Класс платной группы
+	*/
 	class PaidGroup : public Group {
 	private:
+
+		//! Номер контракта
 		int _contract_id;
+
+		//! Размер оплаты
 		float _payment_size;
 
 	public:
+
+		/**
+		* @brief Конструктор по значениям полей
+		* @param id Номер группы
+		* @param size Количество студентов группы
+		* @param department_id Номер профилирующей кафедры
+		* @param study_duration Срок обучения
+		* @param contract_id Номер контракта
+		* @param payment_size Размер оплаты
+		*/
 		PaidGroup(int id, int size, int department_id, int study_duration,
 			int contract_id, float payment_size) :
 			Group(id, size, department_id, study_duration),
 			_contract_id(contract_id),
 			_payment_size(payment_size) {}
 
+		/**
+		* @brief Копирующий конструктор
+		* @param src Ссылка на копируемый объект группы
+		*/
 		PaidGroup(const PaidGroup& src) :
 			Group(src),
 			_contract_id(src._contract_id),
 			_payment_size(src._payment_size) {}
 
+		/**
+		* @brief Перемещающий конструктор
+		* @param src Ссылка на перемещаемый объект группы
+		*/
 		PaidGroup(PaidGroup&& src) :
 			Group(src),
 			_contract_id(src._contract_id),
 			_payment_size(src._payment_size) {}
 
+		/**
+		* @brief Копирующий оператор присваивания
+		* @param src Ссылка на копируемый объект группы
+		* @return Ссылка на скопированный объект
+		*/
 		PaidGroup& operator= (const PaidGroup& src);
 
+		/**
+		* @brief Перемещающий оператор присваивания
+		* @param src Ссылка на перемещаемый объект группы
+		* @return Ссылка на перемещенный объект
+		*/
 		PaidGroup& operator= (PaidGroup&& src);
 
+		/**
+		* @brief Геттер номера контракта
+		* @return Номер контракта
+		*/
 		int contract_id() const;
 
+		/**
+		* @brief Геттер размера оплаты
+		* @return Размер оплаты
+		*/
 		float payment_size() const;
 
+		/**
+		* @brief Сеттер размера оплаты
+		* @param payment_size Новое значение размера оплаты
+		* @return Размер оплаты
+		*/
 		float payment_size(float payment_size);
 
+		/**
+		* @brief Дружественный оператор вывода платной группы в стандартный поток вывода
+		* @param out Ссылка на стандартный поток вывода
+		* @param pg Ссылка на объект платной группы
+		* @return Ссылка на стандартный поток вывода
+		*/
 		friend std::ostream& operator<< (std::ostream& out, const PaidGroup& pg);
 	};
 }
